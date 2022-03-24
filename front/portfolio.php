1f15ee33478ffdc270f272f3bb1c65813e19f03e
Astour44 <astour44@gmail.com>
2022-03-24 15:42:46 +0800

    完成自動增加欄位的作品

--- front/portfolio.php
+++ front/portfolio.php
@@ -1,75 +1,42 @@
 <div class="top">
     <div class="portfoliotext" id="portfoliotextjump"><a href="#Home">My <br>Portfolio</a></div>
 </div>
-<div class="bordergray"></div>
-<div class="top3">
+<?php
+$rows = $Pro->all();
+foreach ($rows as $row => $value) {
+    if ($row % 2 == 0) {
+?>
+        <div class="bordergray"></div>
+        <div class="top3">
+            <div class="portfolio a">
+                <img src="./img/<?= $value['img']; ?>.jpg" alt="" width="100%" height="100%">
 
+            </div>
+            <div class="portfolio">
+                <h1 class="d-flex justify-content-center"></h1>
+                <p class="portfoliop">
+                    <?= $value['text']; ?>
+                </p>
+            </div>
+        </div>
 
     <?php
-    $rows = $Mem->all();
-    foreach ($rows as $row => $value) {
-        if ($row % 2 == 0) {
-            echo "可以被整除";
-        } else {
-            echo "不能被整除";
-        }
-        echo "<br>";
-    }
+    } else {
     ?>
-
-
-    <div class="portfolio a"></div>
-    <div class="portfolio">
-        <h1 class="d-flex justify-content-center">PHP</h1>
-        <p class="portfoliop">
-            Lorem ipsum dolor sit amet, <br>
-            adipisicing elit. Eius, <br>
-            omnis explicaboreprehendert <br>
-            necessitatibus minima qu<br>
-            o vel ipsa, ea tempora alias <br>
-            itaque officiis dolor<br>
-            a, perspiciatis labore iste? Sunt.<br>
-            Lorem ipsum dolor sit amet, <br>
-            adipisicing elit. Eius, <br>
-            omnis explicaboreprehent <br>
-        </p>
-    </div>
-</div>
-<div class="bordergray"></div>
-<div class="top3">
-    <div class="portfolio">
-        <h1 class="d-flex justify-content-center">Design</h1>
-        <p class="portfoliop">
-            Lorem ipsum dolor sit amet, <br>
-            adipisicing elit. Eius, <br>
-            omnis explicaboreprehendert <br>
-            necessitatibus minima qu<br>
-            o vel ipsa, ea tempora alias <br>
-            itaque officiis dolor<br>
-            a, perspiciatis labore iste? Sunt.<br>
-            Lorem ipsum dolor sit amet, <br>
-            adipisicing elit. Eius, <br>
-            omnis explicaboreprehent <br>
-        </p>
-    </div>
-    <div class="portfolio b">portfolio1text</div>
-</div>
-<div class="bordergray"></div>
-<div class="top3">
-    <div class="portfolio c"></div>
-    <div class="portfolio">
-        <h1 class="d-flex justify-content-center">Planning</h1>
-        <p class="portfoliop">
-            Lorem ipsum dolor sit amet, <br>
-            adipisicing elit. Eius, <br>
-            omnis explicaboreprehendert <br>
-            necessitatibus minima qu<br>
-            o vel ipsa, ea tempora alias <br>
-            itaque officiis dolor<br>
-            a, perspiciatis labore iste? Sunt.<br>
-            Lorem ipsum dolor sit amet, <br>
-            adipisicing elit. Eius, <br>
-            omnis explicaboreprehent <br>
-        </p>
-    </div>
-</div>
+        <div class="bordergray"></div>
+        <div class="top3">
+            <div class="portfolio">
+                <h1 class="d-flex justify-content-center"></h1>
+                <p class="portfoliop">
+                    <?= $value['text']; ?>
+                </p>
+            </div>
+            <div class="portfolio b">
+                <img src="./img/<?= $value['img']; ?>.jpg" alt="" width="100%" height="100%">
+            </div>
+        </div>
+<?php
+    }
+    echo "<br>";
+}
+?>

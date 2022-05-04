290b8c26419504f97bdf85a7d8b53d26facab257
Astour44 <astour44@gmail.com>
2022-05-04 18:04:41 +0800

    完成介紹欄

--- front/introduce.php
+++ front/introduce.php
@@ -1,18 +1,31 @@
 <h1 id="Introducejump"><a href="#Home">Introduce</a></h1>
-        <div class="top">
-            <div class="notes">
-                <div class="notesp">
-                    <p style="width:300px;height:230px;">
-                       
-                    </p>
-                    <p style="width:350px;height:230px;">
+<div class="top">
+    <div class="notes">
+    <img src="./img/未命名-1.gif" style="position: absolute;border:1px solid white;border-radius: 10px;" width="1100px" height="400px">
+        <div class="notesp">
+           
+            <p style="width:300px;height:230px;">
+                <img src="./img/html.png" style="border: 2px solid white;margin-left:5px;margin-top:5px;padding:2px" width="75" height="75">
+                <img src="./img/css.png" style="border: 2px solid white;margin-left:5px;margin-top:5px;padding-left:10px;padding-right:10px;padding-top:2px;padding-bottom:2px" width="75" height="75">
+                <img src="./img/JS.png" style="border: 2px solid white;margin-left:5px;margin-top:5px;padding:1px" width="75" height="75">
+                <img src="./img/JQ.png" style="border: 2px solid white;margin-left:5px;margin-top:5px;padding:2px" width="75" height="75">
+                <img src="./img/BT.png" style="border: 2px solid white;margin-left:5px;margin-top:5px;margin-left:5px;margin-top:5px;padding-left:4px;padding-right:4px;padding-top:6px;padding-bottom:6px" width="75" height="75">
+                <img src="./img/PS.png" style="border: 2px solid white;margin-left:5px;margin-top:5px;padding:5px" width="75" height="75">
+                <img src="./img/AI.png" style="border: 2px solid white;margin-left:5px;margin-top:5px;padding:5px" width="75" height="75">
+                <img src="./img/git.png" style="border: 2px solid white;margin-left:5px;margin-top:5px;padding:5px" width="75" height="75">
+                <img src="./img/PHP.png" style="border: 2px solid white;margin-left:5px;margin-top:5px;padding-left:4px;padding-right:4px;padding-top:7px;padding-bottom:7px" width="75" height="75">
+                <img src="./img/MySQL.png" style="border: 2px solid white;margin-left:5px;margin-top:5px;padding-left:4px;padding-right:4px;padding-top:7px;padding-bottom:7px" width="75" height="75">
+                <img src="./img/laravel.png" style="border: 2px solid white;margin-left:5px;margin-top:5px;padding:5px" width="75" height="75">
+               
+            </p>
+            <p style="width:350px;height:230px;">
 
-                        <?= $Int->find(1)['text']; ?>
+                <?= $Int->find(1)['text']; ?>
 
-                    </p>
-                </div>
-                <div class="introduction">
-                    <img src="./img/<?= $Int->find(1)['img']; ?>" width="240" height="290">
-                </div>
-            </div>
+            </p>
         </div>
+        <div class="introduction">
+            <img src="./img/<?= $Int->find(1)['img']; ?>" width="240" height="290">
+        </div>
+    </div>
+</div>
